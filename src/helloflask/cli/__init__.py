de312d7b516c50e132077dab2d712a961434c2cc
Terrel Shumway <gh628@flaskpro.com>
2019-03-07 04:56:37 -0700

    another way to run the server

--- src/helloflask/cli/__init__.py
+++ src/helloflask/cli/__init__.py
@@ -1,6 +1,9 @@
-
 import click
+
 
 @click.command()
 def main():
     click.echo(f"Hello from {__name__}")
+    from helloflask.app import create_app
+    app = create_app()
+    return app.run(debug=True)
